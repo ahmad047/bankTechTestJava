0a68f9d8266f8ec983ea5000f97241fde53ad048
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-08-30 15:56:29 +0100

    transaction objects can now be saved in the bank account

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,15 +1,28 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class BankAccount {
     private float balance;
 
-    public void deposit(float amount) {
+    private ArrayList<Transaction> transactions = new ArrayList();
+
+    public void deposit(float amount, LocalDate date) {
         balance += amount;
+        Transaction transaction = new Transaction(amount, 0, balance, date);
+        this.transactions.add(transaction);
     }
 
-    public void withdraw(float amount) {
+    public void withdraw(float amount, LocalDate date) {
         balance -= amount;
+        Transaction transaction = new Transaction(0, amount, balance, date);
+        this.transactions.add(transaction);
     }
 
     public float getBalance () {
         return balance;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
     }
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,22 +1,42 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankAccountTest {
+    BankAccount subject;
+    @BeforeEach
+    void createBankAccount(){
+        subject = new BankAccount();
+        subject.deposit(500, LocalDate.of(2021, 8, 1));
+    }
     @Test
+    @DisplayName("Test can increase the balance on deposit")
     void depositsFunds(){
-        BankAccount subject = new BankAccount();
-
-        subject.deposit(500);
         assertEquals(500.00, subject.getBalance());
     }
 
     @Test
+    @DisplayName("Test can reduce the balance on withdrawal")
     void withdrawFunds(){
-        BankAccount subject = new BankAccount();
-
-        subject.deposit(500);
-        subject.withdraw(200);
+        subject.withdraw(200, LocalDate.of(2021, 8, 1));
         assertEquals(300.00, subject.getBalance());
+    }
+
+    @Test
+    @DisplayName("Test can create a transaction object on deposit")
+    void savesObjectOnDesposit(){
+        assertEquals(500, subject.getTransactions().get(0).getCredit());
+    }
+
+    @Test
+    @DisplayName("Test can create a transaction object on withdrawal")
+    void savesObjectOnWithdrawal(){
+        subject.withdraw(100, LocalDate.of(2021, 8, 1));
+        assertEquals(100, subject.getTransactions().get(1).getDebit());
+
     }
 }
